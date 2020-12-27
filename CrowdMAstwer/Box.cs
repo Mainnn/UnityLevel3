3d730faaf5e695314c45c9478e3ebcb6eaa4745d
Papi4 <32244487+Mainnn@users.noreply.github.com>
2020-12-27 17:40:23 +0300

    Add files via upload

---
+++ CrowdMAstwer/Box.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Box : MonoBehaviour
+{
+    public event UnityAction Tach;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Objeckt")
+        {
+            Tach?.Invoke();
+        }
+    }
+
+}
---
+++ CrowdMAstwer/BoxExploetion.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxExploetion : MonoBehaviour
+{
+    [SerializeField] private Box _box;
+    [SerializeField] private float _explosionForce;
+    [SerializeField] private float _explosionRadius;
+
+    private Vector3 _center;
+
+    private bool _tachBox;
+
+    private void Start()
+    {
+        _center = _box.gameObject.transform.position;
+    }
+    private void OnEnable()
+    {
+        _box.Tach += Explosion;
+    }
+
+    private void OnDisable()
+    {
+        _box.Tach -= Explosion;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Objeckt" && _tachBox)
+        {
+            Rigidbody rigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Debug.Log(other.name);
+            rigidbody.AddExplosionForce(_explosionForce, _center, _explosionRadius);
+        }
+    }
+
+    private void Explosion()
+    {
+        _tachBox = true;
+
+    }
+}
